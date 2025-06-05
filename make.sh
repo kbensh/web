9cf6ef091bc80bacdeeaefef2f4dbee2e1151471
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-05 12:26:41 +0300

    Use template
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- make.sh
+++ make.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
-set -x
 
 build="build"
-src="html/index.html"
-dest="$build/index.html"
-repo="kben-sh/benchmarks"
+template="html/template.html"
+repo="kbensh/benchmarks"
 
 mkdir -p "$build"
 cp -r img "$build"
 cp -r css "$build"
 cp -r js "$build"
 
-header_content=$(<"header.html")
-footer_content=$(<"footer.html")
 updated="$(LANG=en_us_88591; date +'%R'; date +'%m/%d/%Y')"
 version=$(curl -s "https://api.github.com/repos/$repo/tags" | jq -r '.[0].name' || echo "0.1"); [ "$version" = "null" ] && version="0.1"
 revision=$(curl -s "https://api.github.com/repos/$repo/commits" | jq -r '.[0].sha' | cut -c1-7 || echo "unknown")
@@ -21,17 +16,25 @@ commitmsg=$(curl -s "https://api.github.com/repos/$repo/commits/$revision" | jq 
 
 for md in md/*.md; do
   html=$(basename "$md" .md).html
-  pandoc "$md" --standalone --from markdown --to html5 --output "$build/$html"
+  pandoc "$md" --standalone --from markdown --to html5 --template="$template" --output "$build/$html"
 done
 
-pandoc "$src"                         \
-  --standalone                        \
-  --from html                         \
-  --template "$src"                   \
-  --to html5                          \
-  --variable title="benchmarks.sh"    \
-  --variable version="$version"       \
-  --variable revision="$revision"     \
-  --variable updated="$updated"       \
-  --variable commitmsg="$commitmsg"   \
-  --output "$dest"
+cp html/* build/
+
+buildhtml() {
+  pandoc "$1"                           \
+    --standalone                        \
+    --from html                     \
+    --template "$template"              \
+    --to html5                          \
+    --variable title="kben.sh"          \
+    --variable version="$version"       \
+    --variable revision="$revision"     \
+    --variable updated="$updated"       \
+    --variable commitmsg="$commitmsg"   \
+    --output "$2"
+}
+
+buildhtml "html/index.html" "$build/index.html"
+
+echo "$(date)": "Built website!"
