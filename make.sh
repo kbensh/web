3af769d57c2a935c465fe063d7e73b002864309a
Evangelos Lamprou <vagos@lamprou.xyz>
2025-06-25 14:16:51 +0300

    Remove md files
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- make.sh
+++ make.sh
@@ -15,7 +15,6 @@ updated="$(LANG=en_us_88591; date +'%R'; date +'%m/%d/%Y')"
 version=$(curl -s "https://api.github.com/repos/$repo/tags" | jq -r '.[0].name' || echo "0.1"); [ "$version" = "null" ] && version="0.1"
 revision=$(curl -s "https://api.github.com/repos/$repo/commits" | jq -r '.[0].sha' | cut -c1-7 || echo "unknown")
 commitmsg=$(curl -s "https://api.github.com/repos/$repo/commits/$revision" | jq -r '.commit.message' || echo "unknown")
-logo=$(<./txt/koala.txt)
 
 buildthing() {
   # suffix is .md build from markdown
@@ -31,7 +30,6 @@ buildthing() {
     --template "$template"              \
     --to html5                          \
     --variable title="kben.sh"          \
-    --variable logo="$logo"             \
     --variable version="$version"       \
     --variable revision="$revision"     \
     --variable updated="$updated"       \
@@ -40,7 +38,5 @@ buildthing() {
 }
 
 buildthing "html/index.html" "$build/index.html"
-buildthing "md/data.md"      "$build/data.html"
-buildthing "md/benchmarks.md" "$build/benchmarks.html"
 
 echo "$(date)": "Built website!"
